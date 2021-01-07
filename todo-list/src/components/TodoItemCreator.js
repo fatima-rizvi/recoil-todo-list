57d1e118d22c17aeb738bf291433caff1479ebab
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-06 21:58:54 -0600

    Added notes

--- todo-list/src/components/TodoItemCreator.js
+++ todo-list/src/components/TodoItemCreator.js
@@ -17,6 +17,7 @@ function getId() {
 
 // To create new todo items, we need to access a setter function that can update the contents of the todoListState (the atom we made in the ToDoList component)
 // Use the useSetRecoilState hook to get a setter function in this component
+// Note: We use the updater form of the setter function sp that we can create a new todoList based on the old todo list
 
 function TodoItemCreator({listState}) {
     const [inputValue, setInputValue] = useState('');
