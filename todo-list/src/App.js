5aec4e04d938549694124290ee4e00b3c8d92e22
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-06 21:06:39 -0600

    Ran npm install, imported necessary parts from recoil, and delted uneccessary code from creating the react app

---
+++ todo-list/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
